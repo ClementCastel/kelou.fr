9192cfcd4e88a43b78a8ec73c9aca159a4eda16e
Clément Castel <36570169+Th3Sword290@users.noreply.github.com>
2018-03-13 18:31:05 +0100

    + Some changes
    + Fulls thumbs / preview
    + New url because of the new DN
    + Changes onto code organisation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,162 @@
-!function(e){var a={fullScreenHeader:!0,parallax:!0,parallaxFactor:10};skel.breakpoints({wide:"(max-width: 1680px)",normal:"(max-width: 1080px)",narrow:"(max-width: 840px)",mobile:"(max-width: 736px)"}),e(function(){var l=e(window),o=e("body");if(skel.vars.mobile&&(a.parallax=!1,o.addClass("is-scroll")),o.addClass("is-loading"),l.on("load",function(){o.removeClass("is-loading")}),skel.vars.IEVersion<9&&e(":last-child").addClass("last-child"),e("form").placeholder(),skel.on("+mobile -mobile",function(){e.prioritize(".important\\28 mobile\\29",skel.breakpoint("mobile").active)}),e(".scrolly-middle").scrolly({speed:1e3,anchor:"middle"}),e(".scrolly").scrolly({speed:1e3,offset:function(){return skel.breakpoint("mobile").active?70:190}}),a.fullScreenHeader){var r=e("#header");if(r.length>0){var i=r.find("header");l.on("resize.overflow_fsh",function(){if(skel.breakpoint("mobile").active)r.css("padding","");else{var e=Math.max(192,(l.height()-i.outerHeight())/2);r.css("padding",e+"px 0 "+e+"px 0")}}).trigger("resize.overflow_fsh"),l.load(function(){l.trigger("resize.overflow_fsh")})}}if(("ie"==skel.vars.browser||skel.vars.mobile)&&(a.parallax=!1),a.parallax){var s,n=e();l.on("scroll.overflow_parallax",function(){s.css("background-position","center "+parseInt(l.scrollTop())/a.parallaxFactor*-1+"px")}).on("resize.overflow_parallax",function(){!skel.breakpoint("wide").active||skel.breakpoint("narrow").active?(o.css("background-position",""),s=n):s=o,l.triggerHandler("scroll.overflow_parallax")}).trigger("resize.overflow_parallax")}e(".gallery").poptrox({useBodyOverflow:!1,usePopupEasyClose:!1,overlayColor:"#0a1919",overlayOpacity:skel.vars.IEVersion<9?0:.75,usePopupDefaultStyling:!1,usePopupCaption:!0,popupLoaderText:"",windowMargin:10,usePopupNav:!0})})}(jQuery);
+/*
+	Overflow by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var settings = {
+
+		// Full screen header?
+			fullScreenHeader: true,
+
+		// Parallax background effect?
+			parallax: true,
+
+		// Parallax factor (lower = more intense, higher = less intense).
+			parallaxFactor: 10
+
+	};
+
+	skel.breakpoints({
+		wide: '(max-width: 1680px)',
+		normal: '(max-width: 1080px)',
+		narrow: '(max-width: 840px)',
+		mobile: '(max-width: 736px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		if (skel.vars.mobile) {
+
+			settings.parallax = false;
+			$body.addClass('is-scroll');
+
+		}
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+
+		// Scrolly links.
+			$('.scrolly-middle').scrolly({
+				speed: 1000,
+				anchor: 'middle'
+			});
+
+			$('.scrolly').scrolly({
+				speed: 1000,
+				offset: function() { return (skel.breakpoint('mobile').active ? 70 : 190); }
+			});
+
+		// Full screen header.
+			if (settings.fullScreenHeader) {
+
+				var $header = $('#header');
+
+				if ($header.length > 0) {
+
+					var $header_header = $header.find('header');
+
+					$window
+						.on('resize.overflow_fsh', function() {
+
+							if (skel.breakpoint('mobile').active)
+								$header.css('padding', '');
+							else {
+
+								var p = Math.max(192, ($window.height() - $header_header.outerHeight()) / 2);
+								$header.css('padding', p + 'px 0 ' + p + 'px 0');
+
+							}
+
+						})
+						.trigger('resize.overflow_fsh');
+
+					$window.load(function() {
+						$window.trigger('resize.overflow_fsh');
+					});
+
+				}
+
+			}
+
+		// Parallax background.
+
+			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+				if (skel.vars.browser == 'ie'
+				||	skel.vars.mobile)
+					settings.parallax = false;
+
+			if (settings.parallax) {
+
+				var $dummy = $(), $bg;
+
+				$window
+					.on('scroll.overflow_parallax', function() {
+
+						// Adjust background position.
+							$bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+
+					})
+					.on('resize.overflow_parallax', function() {
+
+						// If we're in a situation where we need to temporarily disable parallax, do so.
+							if (!skel.breakpoint('wide').active
+							||	skel.breakpoint('narrow').active) {
+
+								$body.css('background-position', '');
+								$bg = $dummy;
+
+							}
+
+						// Otherwise, continue as normal.
+							else
+								$bg = $body;
+
+						// Trigger scroll handler.
+							$window.triggerHandler('scroll.overflow_parallax');
+
+					})
+					.trigger('resize.overflow_parallax');
+
+			}
+
+		// Poptrox.
+			$('.gallery').poptrox({
+				useBodyOverflow: false,
+				usePopupEasyClose: false,
+				overlayColor: '#0a1919',
+				overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
+				usePopupDefaultStyling: false,
+				usePopupCaption: true,
+				popupLoaderText: '',
+				windowMargin: 10,
+				usePopupNav: true
+			});
+
+	});
+
+})(jQuery);
